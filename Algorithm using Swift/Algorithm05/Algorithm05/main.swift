a7bdfe4ca8f4150fa5b30bb8e711e78ac0d9c588
AnnaBaeTofuMom <mymininic@gmail.com>
2022-05-24 00:05:17 +0900

    [MOD] Algorithm05 : 삽입정렬 이론 및 소스코드 추가

--- Algorithm using Swift/Algorithm05/Algorithm05/main.swift
+++ Algorithm using Swift/Algorithm05/Algorithm05/main.swift
@@ -11,20 +11,38 @@ import Foundation
 
 //선택정렬
 
-var array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+var selectionArray = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in 0...array.count - 1 {
+for i in 0...selectionArray.count - 1 {
     var minIndex = i //가장 작은 원소의 인덱스
-    for j in i + 1..<array.count {
-        if array[minIndex] > array[j] {
+    for j in i + 1..<selectionArray.count {
+        if selectionArray[minIndex] > selectionArray[j] {
             minIndex = j
 
         }
     }
-    var frontNum = array[i]
-    var changeNum = array[minIndex]
-    array[i] = changeNum
-    array[minIndex] = frontNum
+    var frontNum = selectionArray[i]
+    var changeNum = selectionArray[minIndex]
+    selectionArray[i] = changeNum
+    selectionArray[minIndex] = frontNum
 }
 
-print(array)
+print(selectionArray)
+
+//삽입 정렬
+
+var insertionArray = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in 1..<insertionArray.count {
+    for j in stride(from: i, to: 0, by: -1) {
+        if insertionArray[j] < insertionArray[j - 1] {
+            insertionArray.swapAt(j, j - 1)
+        } else {
+            break
+        }
+    }
+}
+print(insertionArray)
+
+
+
