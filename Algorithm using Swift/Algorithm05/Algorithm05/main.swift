cc627896d9df8e15c2be6cbc007ca8f696074d11
AnnaBaeTofuMom <mymininic@gmail.com>
2022-05-23 23:47:52 +0900

    [MOD] Algorithm05 : 선택정렬 소스코드와 시간복잡도

--- Algorithm using Swift/Algorithm05/Algorithm05/main.swift
+++ Algorithm using Swift/Algorithm05/Algorithm05/main.swift
@@ -8,3 +8,23 @@
 import Foundation
 
 //sort 문제풀이를 위한 project
+
+//선택정렬
+
+var array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in 0...array.count - 1 {
+    var minIndex = i //가장 작은 원소의 인덱스
+    for j in i + 1..<array.count {
+        if array[minIndex] > array[j] {
+            minIndex = j
+
+        }
+    }
+    var frontNum = array[i]
+    var changeNum = array[minIndex]
+    array[i] = changeNum
+    array[minIndex] = frontNum
+}
+
+print(array)
