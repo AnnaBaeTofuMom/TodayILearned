3e757defa8484087bc3e0b17e780fa3fb181146e
Hi, Anna ♡ <61861862+AnnaBaeTofuMom@users.noreply.github.com>
2022-06-21 01:36:51 +0900

    [MOD] Swift : 값 타입 해제 실험

--- Swift/Reference_Type_Value_Type/Reference_Type_Value_Type/main.swift
+++ Swift/Reference_Type_Value_Type/Reference_Type_Value_Type/main.swift
@@ -23,11 +23,11 @@ class Person {
 
 var person: Person? = Person()
 
-
 // weak person과 person 의 차이 확인해보기
 // 강한참조로 캡쳐를 할때 참조 카운트가 +1 되기 때문에 아래서 nil로 해제해줘도 카운트가 +1이 됨
 // 그래서 person을 nil을 해줘도 mama가 프린트 되는 상황이 됨.
 // 약한 참조를 해주면 참조 카운트가 올라가지 않기 때문에 nil을 해주면 완전히 해제되어 nil이 프링트 됨
+
 let closure = { [weak person] in
     print(person?.name)
 }
