c53b205e9f3686d6e669eae507b4bf107348997d
Hi, Anna ♡ <61861862+AnnaBaeTofuMom@users.noreply.github.com>
2022-06-20 19:37:51 +0900

    [ADD] Swift : 값 타입 참조타입의 클로저 활용
    값타입, 참조타입의 클로저 내부 동작을 이해

---
+++ Swift/Reference_Type_Value_Type/Reference_Type_Value_Type/main.swift
@@ -0,0 +1,64 @@
+//
+//  main.swift
+//  Reference_Type_Value_Type
+//
+//  Created by 경원이 on 2022/06/20.
+//
+
+import Foundation
+
+//참조타입일때는 캡쳐 당시에 주소값을 저장하는듯?
+
+class Person {
+    var name: String?
+
+    func printName() {
+        print("my name is \(self.name)")
+    }
+
+    init() {
+        self.name = "Anna"
+    }
+}
+
+var person: Person? = Person()
+
+let closure = { [weak person] in
+    print(person?.name)
+}
+
+person?.name = "mama"
+
+closure() //mama
+
+person = nil
+
+closure() //nil
+
+
+// 값타입은 캡쳐 당시의 값을 유지
+//struct Person {
+//    var name: String?
+//
+//    func printName() {
+//        print("my name is \(self.name)")
+//    }
+//
+//    init() {
+//        self.name = "Anna"
+//    }
+//}
+//
+//var person: Person? = Person()
+//
+//let closure = { [person] in
+//    print(person?.name)
+//}
+//
+//person?.name = "mama"
+//
+//closure() //Anna
+//
+//person = nil
+//
+//closure() //Anna
